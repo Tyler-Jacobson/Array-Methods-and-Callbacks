ea534467bbca695a8df38bdffdd426618f719b09
Tyler Jacobson <TylerJacobsonSE@gmail.com>
2020-08-18 08:18:26 -0700

    Test commit

--- index.js
+++ index.js
@@ -15,11 +15,11 @@ console.log('its working');
 
 /* Task 2: Create a function called  getFinals that takes `data` as an argument and returns an array of objects with only finals data */
 
-function getFinals(/* code here */) {
+function getFinals(data) {
 
-    /* code here */
-
-};
+    const mappedData = data.map((state) => {
+        return {"stage": state.final, "year": state.year}
+});
 
 /* Task 3: Implement a higher-order function called `getYears` that accepts the callback function `getFinals`, and returns an array called `years` containing all of the years in the dataset */
 
